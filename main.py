413eaaa64980ca6694277fa71dc7f61f54e96bb1
XE8971\XE <marinaburda053@gmail.com>
2023-04-22 14:46:17 +0300

    Connect to database and getting chats_id

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from pprint import pprint
+from telegram import Update, Bot, ForceReply
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Application
+
+from services.DatabaseRequests import DatabaseRequests
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+class TelegramBot:
+    
+    def __init__(self):
+        self.__database = DatabaseRequests()
+        self.__bot = Bot("5957878793:AAH391KCu0twpoXkbc7kfTzbsXNnN_Qc-5A")
 
 
-app = ApplicationBuilder().token("5957878793:AAH391KCu0twpoXkbc7kfTzbsXNnN_Qc-5A").build()
+    async def start(self, update: Update,
+                    context: ContextTypes.DEFAULT_TYPE) -> None:
+  
+        user = update.effective_user
+        await update.message.reply_html(
+            rf"Hi, {user.mention_html()}!",
+            reply_markup=ForceReply(selective=True),
+        )
 
-app.add_handler(CommandHandler("hello", hello))
+        if context.bot_data.get("chats_id") == None:
+            context.bot_data.update({"chats_id": list()})
 
-app.run_polling()
+        chats_id = context.bot_data.get("chats_id")
+
+        if update.message.chat.id not in chats_id:
+            chats_id.append(update.message.chat_id)
+            context.bot_data.update({"chats_id": chats_id})
+        pprint(context.bot_data.get("chats_id"))
+
+
+    async def echo(self, update: Update,
+                   context: ContextTypes.DEFAULT_TYPE) -> None:
+        
+        await update.message.reply_text(update.message.text)
+
+
+    async def help_command(self, update: Update,
+                           context: ContextTypes.DEFAULT_TYPE) -> None:
+        
+        await update.message.reply_text("Help!")
+
+
+    def main(self) -> None:
+
+        application = Application.builder().token(
+            "5957878793:AAH391KCu0twpoXkbc7kfTzbsXNnN_Qc-5A").build()
+        application.add_handler(CommandHandler("start", self.start))
+        application.add_handler(CommandHandler("help", self.help_command))
+        application.run_polling()
+
+
+telebot = TelegramBot()
+telebot.main()
