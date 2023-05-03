1b026dc993a7ec64c3dabb36983f219e9d5b0348
XE8971\XE <marinaburda053@gmail.com>
2023-05-03 20:23:09 +0300

    Change db request

--- services/DatabaseRequests.py
+++ services/DatabaseRequests.py
@@ -1,27 +1,4 @@
-# import mysql.connector
-
-
-# class DatabaseRequests:
-#     def __init__(self):
-#         self.cnx = mysql.connector.connect(user='pythonuser', password='HFS#*3fdsJDS', host='151.115.32.54',
-#                                            database='bot')
-#         self.cursor = self.cnx.cursor()
-
-#     def select(self, query):
-#         self.cursor.execute(query)
-#         return self.cursor.fetchall()
-
-#     def insert(self, query, values):
-#         self.cursor.execute(query, values)
-#         self.cnx.commit()
-#         return self.cursor.lastrowid
-
-#     def update(self, query):
-#         self.cursor.execute(query)
-#         self.cnx.commit()
-
 import aiomysql
-
 
 class DatabaseRequests:
     def __init__(self):
